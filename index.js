cc8d112718bd7bbc10c27004b77abf0dc031c9d7
Theo Booth <theorlbooth@googlemail.com>
2020-02-28 10:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  return newObj
+}
