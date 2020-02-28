4a6bc383471e7231989ba8b356eed3b0898a332f
Theo Booth <theorlbooth@googlemail.com>
2020-02-28 10:44:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  return Object.assign({}), object, {[key]: value})
 }
